584adcff5aeb1328b7edb6a0f84a06a545c5854e
ThunderM200 <martin.sandoval@pregrado.uoh.cl>
2026-07-18 00:18:33 -0400

    Updated compile script to build output folder if it is missing.

--- document-compile.sh
+++ document-compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+###### For selecting directory of the repository as pwd
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$SCRIPT_DIR/src" || exit 1
+mkdir -p "$SCRIPT_DIR/output"
 
+cd "$SCRIPT_DIR/src" || exit 1
+######
+
+###### For running the compile command. Modify compiler as needed.
+###### Try not to change output directory or main.tex.
 pdflatex main.tex -output-directory=../output
